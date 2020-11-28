12e018829ee3252a2b2c7ce13b9508badb8cc8b2
luis-code <rockband_666@hotmail.com>
2020-11-28 19:47:25 -0400

    actualizacion

--- punto_fijo.py
+++ punto_fijo.py
@@ -1,39 +1,56 @@
-# Algoritmo de punto fijo
-# [a,b] intervalo de búsqueda
-# error = tolera
+expresion_fx = None
 
-import numpy as np
-def puntofijo(gx,a,tolera, iteramax = 15):
-    i = 1 # iteración
-    b = gx(a)
-    tramo = abs(b-a)
-    while(tramo>=tolera and i<=iteramax ):
-        a = b
+def metodo_punto_fijo():
+    # Algoritmo de punto fijo
+    # [a,b] intervalo de búsqueda
+    # error = tolera
+    global expresion_fx
+    import numpy as np
+
+    # INGRESO
+    #fx = lambda x: np.exp(-x) - x
+    gx = lambda x: np.exp(-x)
+
+    a = 0       # intervalo
+    b = 1
+    tolera = 0.001
+    iteramax  = 15      # itera máximo
+    muestras = 51  # gráfico
+
+    tramos = 51
+
+    expresion_fx = input("Ingrese f(x), ej: (exp(x) - x) >> ")
+    a = float(input("a, ej: 0 >> "))
+    #p = float(input("m, ej: 1.2 >> "))
+        
+    def f(x):
+        global expresion_fx
+        return ne.evaluate(expresion_fx)
+
+
+
+    def puntofijo(gx,a,tolera, iteramax = 15):
+        i = 1 # iteración
         b = gx(a)
         tramo = abs(b-a)
-        i = i+1
-    respuesta = b
-    # Validar respuesta
-    if (i>=iteramax ):
-        respuesta = np.nan
-    return(respuesta)
+        while(tramo>=tolera and i<=iteramax ):
+            a = b
+            b = gx(a)
+            tramo = abs(b-a)
+            i = i+1
+        respuesta = b
+        # Validar respuesta
+        if (i>=iteramax ):
+            respuesta = np.nan
+        return(respuesta)
 
-# PROGRAMA ---------
+    # PROGRAMA ---------
 
-# INGRESO
-fx = lambda x: np.exp(-x) - x
-gx = lambda x: np.exp(-x)
+    
 
-a = 0       # intervalo
-b = 1
-tolera = 0.001
-iteramax  = 15      # itera máximo
-muestras = 51  # gráfico
+    # PROCEDIMIENTO
+    respuesta = puntofijo(gx,a,tolera)
 
-tramos = 51
-
-# PROCEDIMIENTO
-respuesta = puntofijo(gx,a,tolera)
-
-# SALIDA
-print(respuesta)
+    # SALIDA
+    print(respuesta)
+metodo_punto_fijo()
